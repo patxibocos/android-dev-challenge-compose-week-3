bbac90123b98419d77f861541e6358286fca9866
Patxi Bocos <patxi952@gmail.com>
2021-03-16 16:55:15 +0100

    FavoriteItem added

--- app/src/main/java/com/example/androiddevchallenge/ui/screen/Home.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screen/Home.kt
@@ -15,15 +15,23 @@
  */
 package com.example.androiddevchallenge.ui.screen
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.androiddevchallenge.R
@@ -65,5 +73,32 @@ fun Home(modifier: Modifier = Modifier, darkTheme: Boolean = false) {
             style = MaterialTheme.typography.h2,
             modifier = Modifier.firstBaselineToTop(48.dp, 8.dp)
         )
+    }
+}
+
+@Composable
+fun FavoriteItem(text: String, @DrawableRes drawableResId: Int) {
+    Surface(
+        modifier = Modifier
+            .size(192.dp, 56.dp)
+            .clip(MaterialTheme.shapes.small)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .aspectRatio(1f),
+                painter = painterResource(id = drawableResId),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = text,
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
     }
 }
